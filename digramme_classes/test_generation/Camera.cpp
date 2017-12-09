a8172b8143c31f0252f1bfa6bf45e9cbdc76cd36
Untailygean <toto.lecon4@laposte.net>
2017-12-09 17:22:19 +0100

    mise à jour diagramme + test génération du squelette

---
+++ digramme_classes/test_generation/Camera.cpp
@@ -0,0 +1,8 @@
+
+#include "Camera.h"
+
+void Camera::followProtag() {
+  // Bouml preserved body begin 0001F708
+  // Bouml preserved body end 0001F708
+}
+
---
+++ digramme_classes/test_generation/LevelExit.cpp
@@ -0,0 +1,9 @@
+
+#include "LevelExit.h"
+#include "Object.h"
+
+void LevelExit::endLevel() {
+  // Bouml preserved body begin 0001F688
+  // Bouml preserved body end 0001F688
+}
+
