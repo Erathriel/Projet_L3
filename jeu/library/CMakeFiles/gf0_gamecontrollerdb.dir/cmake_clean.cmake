f1f9e267be5222993d0d54c4b4083c131d990acf
Untailygean <toto.lecon4@laposte.net>
2018-01-18 17:56:29 +0100

    suppression des cmakesfiles de github

--- jeu/library/CMakeFiles/gf0_gamecontrollerdb.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gf0_gamecontrollerdb"
-  "generated/gamecontrollerdb.txt.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gf0_gamecontrollerdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
